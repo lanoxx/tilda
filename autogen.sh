e8e91a694867f585ddc764b9513dac378861d39e
Sebastian Geiger <sbastig@gmx.net>
2012-09-14 22:48:35 +0200

    Update autotools files
     * autogen.sh: Add some more comments to
       explain what the script actually does
     * autogen.sh: Call autoreconf instead of
       all the individual tools
     * autogen.sh: Also call ./configure from
       this script.
     * configure.ac: Update comments
     * configure.ac: Remove some obsolete macros

--- autogen.sh
+++ autogen.sh
@@ -4,10 +4,11 @@
 # for our purposes. Thanks guys!
 
 echo "This will do all the autotools stuff so that you can build"
-echo "tilda successfully."
+echo "tilda successfully. It will also call the ./configure script"
+echo "and pass on any options which you passed to this script"
+echo "See ./configure --help to know which options are available"
 echo
 echo "When it is finished, take the usual steps to install:"
-echo "./configure"
 echo "make"
 echo "make install"
 echo
@@ -29,12 +30,13 @@ echo
 echo "Generating configuration files for tilda, please wait..."
 echo;
 
-# Gettext
-autopoint --force || exit;
+# Autoconf will call run autopoint, aclocal, autoconf, autoheader and automake
+# to setup and configure the build environment
+autoreconf --verbose --install --symlink
+# Afterwards we invoke the configure skript, "$@" will contain the arguments that
+# were passed to this skript.
 
-# "Normal" Autoconf
-aclocal -I m4 || exit;
-autoconf --force || exit;
-autoheader --force || exit;
-automake --add-missing --force-missing || exit;
-
+echo
+echo "Running configure now"
+echo
+./configure "$@"
