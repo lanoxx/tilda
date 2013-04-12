c7586ccdba5c3c371fe4b14d9b1f76197148dd03
Sebastian Geiger <sbastig@gmx.net>
2013-04-13 00:39:58 +0200

    configsys: Add functions to handle float/double values

--- src/configsys.h
+++ src/configsys.h
@@ -15,16 +15,20 @@ gint config_free (const gchar *config_file);
 gint config_write (const gchar *config_file);
 
 /* Set values in the config system */
-gint config_setint (const gchar *key, const gint val);
-gint config_setstr (const gchar *key, const gchar *val);
-gint config_setbool(const gchar *key, const gboolean val);
-gint config_setnint(const gchar *key, const gint val, const guint idx);
+gint config_setint     (const gchar *key, const gint val);
+gint config_setstr     (const gchar *key, const gchar *val);
+gint config_setbool    (const gchar *key, const gboolean val);
+gint config_setnint    (const gchar *key, const gint val, const guint idx);
+gint config_setdouble  (const gchar *key, const gdouble val);
+gint config_setndouble (const gchar *key, const gdouble vat, const guint idx);
 
 /* Get values from the config system */
-gint     config_getint (const gchar *key);
-gchar*   config_getstr (const gchar *key);
-gboolean config_getbool(const gchar *key);
-glong    config_getnint(const gchar *key, const guint idx);
+gint     config_getint     (const gchar *key);
+gchar*   config_getstr     (const gchar *key);
+gboolean config_getbool    (const gchar *key);
+glong    config_getnint    (const gchar *key, const guint idx);
+gdouble  config_getdouble  (const gchar *key);
+gdouble  config_getndouble (const gchar *key, const guint idx);
 
 #endif /* CONFIGSYS_H */
 
