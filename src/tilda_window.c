lanoxx/tilda

commit 09b7318bb8b7e9c4c61644223e08593740711e3f

Fix copy/paste bug when scrollbar is on the left
This patch fixes a bug that caused the copy and paste
hotkeys to become broken, when the scrollbar was made visible
on the left. With the scrollbar on the right or disabled this
bug was not present.

The reason was that we looked up the vte_terminal widget
based on its location in its parent container and assumed the terminal
widget was always the first child of its parent. If the scrollbar is
on the left then this assumption was not true.

The solution is to lookup the VteTerminal widget from list of
tilda terminals, which keep a pointer to the VteTerminal widget.

src/tilda_window.c
@@ -409,14 +409,15 @@ static gint ccopy (tilda_window *tw)
     DEBUG_ASSERT (tw != NULL);
     DEBUG_ASSERT (tw->notebook != NULL);
 
-    GtkWidget *current_page;
-    GList *list;
-
     gint pos = gtk_notebook_get_current_page (GTK_NOTEBOOK (tw->notebook));
-    current_page = gtk_notebook_get_nth_page (GTK_NOTEBOOK (tw->notebook), pos);
-    list = gtk_container_get_children (GTK_CONTAINER(current_page));
-    vte_terminal_copy_clipboard (VTE_TERMINAL(list->data));
-
+    int counter = 0;
+    for (GList *item=tw->terms; item != NULL; item=item->next) {
+        if (counter == pos) {
+            tilda_term *term = item->data;
+            vte_terminal_copy_clipboard (VTE_TERMINAL(term->vte_term));
+        }
+        counter++;
+    }
     /* Stop the event's propagation */
     return TRUE;
 }
@@ -427,13 +428,15 @@ static gint cpaste (tilda_window *tw)
     DEBUG_ASSERT (tw != NULL);
     DEBUG_ASSERT (tw->notebook != NULL);
 
-    GtkWidget *current_page;
-    GList *list;
-
     gint pos = gtk_notebook_get_current_page (GTK_NOTEBOOK (tw->notebook));
-    current_page = gtk_notebook_get_nth_page (GTK_NOTEBOOK (tw->notebook), pos);
-    list = gtk_container_get_children (GTK_CONTAINER (current_page));
-    vte_terminal_paste_clipboard (VTE_TERMINAL(list->data));
+    int counter = 0;
+    for (GList *item=tw->terms; item != NULL; item=item->next) {
+        if (counter == pos) {
+            tilda_term *term = item->data;
+            vte_terminal_paste_clipboard (VTE_TERMINAL(term->vte_term));
+        }
+        counter++;
+    }
 
     /* Stop the event's propagation */
     return TRUE;

commit 09b8cda2cbd15bb115bdcc9bdfc2036d53aaa5b7

Fix copy/paste hotkeys not working after tab is relocated
Fixes #168
Fixes #167

This was broken by commit 09b7318bb8b7e9c4c61644223e08593740711e3f

Signed-off-by: Sebastian Geiger <sbastig@gmx.net>

src/tilda_window.c
@@ -409,15 +409,17 @@ static gint ccopy (tilda_window *tw)
     DEBUG_ASSERT (tw != NULL);
     DEBUG_ASSERT (tw->notebook != NULL);
 
+    GtkWidget *current_page;
+    GList *list;
+
     gint pos = gtk_notebook_get_current_page (GTK_NOTEBOOK (tw->notebook));
-    int counter = 0;
-    for (GList *item=tw->terms; item != NULL; item=item->next) {
-        if (counter == pos) {
-            tilda_term *term = item->data;
-            vte_terminal_copy_clipboard (VTE_TERMINAL(term->vte_term));
-        }
-        counter++;
+    current_page = gtk_notebook_get_nth_page (GTK_NOTEBOOK (tw->notebook), pos);
+    list = gtk_container_get_children (GTK_CONTAINER(current_page));
+    if GTK_IS_SCROLLBAR(list->data) {
+        list = list->next;
     }
+    vte_terminal_copy_clipboard (VTE_TERMINAL(list->data));
+
     /* Stop the event's propagation */
     return TRUE;
 }
@@ -428,15 +430,16 @@ static gint cpaste (tilda_window *tw)
     DEBUG_ASSERT (tw != NULL);
     DEBUG_ASSERT (tw->notebook != NULL);
 
+    GtkWidget *current_page;
+    GList *list;
+
     gint pos = gtk_notebook_get_current_page (GTK_NOTEBOOK (tw->notebook));
-    int counter = 0;
-    for (GList *item=tw->terms; item != NULL; item=item->next) {
-        if (counter == pos) {
-            tilda_term *term = item->data;
-            vte_terminal_paste_clipboard (VTE_TERMINAL(term->vte_term));
-        }
-        counter++;
+    current_page = gtk_notebook_get_nth_page (GTK_NOTEBOOK (tw->notebook), pos);
+    list = gtk_container_get_children (GTK_CONTAINER (current_page));
+    if GTK_IS_SCROLLBAR(list->data) {
+        list = list->next;
     }
+    vte_terminal_paste_clipboard (VTE_TERMINAL(list->data));
 
     /* Stop the event's propagation */
     return TRUE;