d5690a15927b79724464940f0218d04b8909d29a
pik <alexander.maznev@gmail.com>
2015-06-28 19:02:16 +0800

    fix copy/paste hotkeys not working after tab is relocated

--- src/tilda_window.c
+++ src/tilda_window.c
@@ -552,15 +552,17 @@ static gint ccopy (tilda_window *tw)
     DEBUG_ASSERT (tw != NULL);
     DEBUG_ASSERT (tw->notebook != NULL);
 
+    GtkWidget *current_page;
+    GList *list;
+
     gint pos = gtk_notebook_get_current_page (GTK_NOTEBOOK (tw->notebook));
-    int counter = 0;
-    for (GList *item=tw->terms; item != NULL; item=item->next) {
-        if (counter == pos) {
-            tilda_term *term = item->data;
-            vte_terminal_copy_clipboard (VTE_TERMINAL(term->vte_term));
-        }
-        counter++;
+    current_page = gtk_notebook_get_nth_page (GTK_NOTEBOOK (tw->notebook), pos);
+    list = gtk_container_get_children (GTK_CONTAINER(current_page));
+    if GTK_IS_SCROLLBAR(list->data) {
+        list = list->next;
     }
+    vte_terminal_copy_clipboard (VTE_TERMINAL(list->data));
+
     /* Stop the event's propagation */
     return TRUE;
 }
@@ -571,15 +573,16 @@ static gint cpaste (tilda_window *tw)
     DEBUG_ASSERT (tw != NULL);
     DEBUG_ASSERT (tw->notebook != NULL);
 
+    GtkWidget *current_page;
+    GList *list;
+
     gint pos = gtk_notebook_get_current_page (GTK_NOTEBOOK (tw->notebook));
-    int counter = 0;
-    for (GList *item=tw->terms; item != NULL; item=item->next) {
-        if (counter == pos) {
-            tilda_term *term = item->data;
-            vte_terminal_paste_clipboard (VTE_TERMINAL(term->vte_term));
-        }
-        counter++;
+    current_page = gtk_notebook_get_nth_page (GTK_NOTEBOOK (tw->notebook), pos);
+    list = gtk_container_get_children (GTK_CONTAINER (current_page));
+    if GTK_IS_SCROLLBAR(list->data) {
+        list = list->next;
     }
+    vte_terminal_paste_clipboard (VTE_TERMINAL(list->data));
 
     /* Stop the event's propagation */
     return TRUE;
