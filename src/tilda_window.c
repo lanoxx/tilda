lanoxx/tilda

commit 47c679fb1fcc8aa6817c089bd97ec2064e04a8f1

Search: Initial implementation for a rudimentary search widget in tilda.
Fixes #159

src/tilda_window.c
@@ -660,6 +660,27 @@ static gint tilda_window_setup_keyboard_accelerators (tilda_window *tw)
     return 0;
 }
 
+static tilda_term* tilda_window_get_current_terminal (tilda_window *tw) {
+    gint pos = gtk_notebook_get_current_page (GTK_NOTEBOOK (tw->notebook));
+    if (pos >= 0) {
+        GList *found = g_list_nth (tw->terms, (guint) pos);
+        if (found) {
+            return found->data;
+        }
+    }
+    return NULL;
+}
+
+static void tilda_window_search_cb (GtkButton *button, tilda_window *tw) {
+    tilda_term* term = tilda_window_get_current_terminal (tw);
+    GtkWidget *vteTerminal = term->vte_term;
+    const char* search_text = gtk_entry_buffer_get_text (GTK_ENTRY_BUFFER (gtk_entry_get_buffer (GTK_ENTRY (tw->search_entry))));
+    GError *error = NULL;
+    GRegex *regex = g_regex_new (search_text, G_REGEX_CASELESS, G_REGEX_MATCH_NEWLINE_ANY, &error);
+    vte_terminal_search_set_gregex (VTE_TERMINAL (vteTerminal), regex);
+    vte_terminal_search_find_next (VTE_TERMINAL (vteTerminal));
+}
+
 static gint tilda_window_set_icon (tilda_window *tw, gchar *filename)
 {
     GdkPixbuf *window_icon = gdk_pixbuf_new_from_file (filename, NULL);
@@ -767,8 +788,6 @@ gboolean tilda_window_init (const gchar *config_file, const gint instance, tilda
     tilda_window_set_icon (tw, g_build_filename (DATADIR, "pixmaps", "tilda.png", NULL));
     tilda_window_setup_alpha_mode (tw);
 
-    gtk_widget_set_app_paintable(GTK_WIDGET(tw->window), TRUE);
-
     /* Add keyboard accelerators */
     tw->accel_group = NULL; /* We can redefine the accelerator group from the wizard; this shows that it's our first time defining it. */
     tilda_window_setup_keyboard_accelerators (tw);
@@ -840,11 +859,29 @@ gboolean tilda_window_init (const gchar *config_file, const gint instance, tilda
      * of tilda_terms in the tw->terms structure in sync with the order of tabs. */
     g_signal_connect (G_OBJECT(tw->notebook), "page-reordered", G_CALLBACK (page_reordered_cb), tw);
 
-    /* Add the notebook to the window */
-    gtk_container_add (GTK_CONTAINER(tw->window), tw->notebook);
+    /* Setup the window widget:
+     *  * One vertical box for the notebook and the search bar.
+     *  * The search bar is a horizontal box with the search widgets.
+     *  * The search widgets are a label, a text entry, and a "find" button.
+     */
+    GtkWidget *main_box = gtk_box_new (GTK_ORIENTATION_VERTICAL, 0);
+    GtkWidget *search_box = gtk_box_new (GTK_ORIENTATION_HORIZONTAL, 0);
+
+    GtkWidget *label = gtk_label_new ("Search: ");
+    tw->search_entry = gtk_entry_new ();
+    GtkWidget *find_button = gtk_button_new_with_mnemonic ("_Find");
+    gtk_box_pack_start (GTK_BOX (search_box), label, TRUE, TRUE, 0);
+    gtk_box_pack_start (GTK_BOX (search_box), tw->search_entry, TRUE, TRUE, 0);
+    gtk_box_pack_start (GTK_BOX (search_box), find_button, TRUE, TRUE, 0);
+
+    gtk_container_add (GTK_CONTAINER(tw->window), main_box);
+    gtk_box_pack_start (GTK_BOX (main_box), tw->notebook, TRUE, TRUE, 0);
+    gtk_box_pack_start (GTK_BOX (main_box), search_box, TRUE, TRUE, 0);
+
+    g_signal_connect (G_OBJECT (find_button), "clicked", G_CALLBACK (tilda_window_search_cb), tw);
 
     /* Show the widgets */
-    gtk_widget_show (tw->notebook);
+    gtk_widget_show_all (main_box);
     /* the tw->window widget will be shown later, by pull() */
 
     /* Position the window */

src/tilda_window.h
@@ -35,6 +35,8 @@ struct tilda_window_
 {
     GtkWidget *window;
     GtkWidget *notebook;
+    GtkWidget *search_entry;
+
     GList *terms;
     GtkAccelGroup * accel_group;
     GtkWidget *wizard_window; /* GtkDialog that contains the wizard */

commit 95187b352fa44ec5304c27b5f43315e1451adad8

Fix transparent background in gnome-shell
Unfortunately this was broken by commit 47c679fb1fcc8aa6817c089bd97ec2064e04a8f1.

Fixes #205

src/tilda_window.c
@@ -938,6 +938,8 @@ gboolean tilda_window_init (const gchar *config_file, const gint instance, tilda
     tilda_window_set_icon (tw, g_build_filename (DATADIR, "pixmaps", "tilda.png", NULL));
     tilda_window_setup_alpha_mode (tw);
 
+    gtk_widget_set_app_paintable (GTK_WIDGET (tw->window), TRUE);
+
     /* Add keyboard accelerators */
     tw->accel_group = NULL; /* We can redefine the accelerator group from the wizard; this shows that it's our first time defining it. */
     tilda_window_setup_keyboard_accelerators (tw);