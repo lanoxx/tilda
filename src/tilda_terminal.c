add650a571c3229c916499982309eb4243c0fbc3
pik <alexander.maznev@gmail.com>
2016-04-18 11:22:43 +0800

    Fix issues with close-tab
     * Fixes close-tab menu item not working by partially reverting commit
       35252b2417f68fc4845622d12ee3df6e12a5e1a2.
     * Fixes #229

--- src/tilda_terminal.c
+++ src/tilda_terminal.c
@@ -888,9 +888,6 @@ static void on_popup_hide (GtkWidget *widget, tilda_window *tw)
     DEBUG_ASSERT(widget != NULL);
     DEBUG_ASSERT(tw != NULL);
 
-    GAction *action = g_action_map_lookup_action (G_ACTION_MAP (tw->action_group), "close-tab");
-    g_simple_action_set_enabled (G_SIMPLE_ACTION (action), FALSE);
-
     tw->disable_auto_hide = FALSE;
 }
 
@@ -953,7 +950,6 @@ static void popup_menu (tilda_window *tw, tilda_term *tt)
 
     /* Create the action group */
     GSimpleActionGroup *action_group = g_simple_action_group_new();
-    tw->action_group = action_group;
 
     /* We need two different lists of entries because the
      * because the actions have different scope, some concern the
