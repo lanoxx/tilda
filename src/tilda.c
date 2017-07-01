lanoxx/tilda

commit c85c87ff3fce9a637c3519f67485d0065e65f49e

Add command-line option to use specific config file
A new option can be called, either as `-g` or as `--config-file`
and followed by a path, to specify a configuration file to use for
a Tilda instance. If there is no file at the specified path,
the default config file for the current instance is used.

src/tilda.c
@@ -310,13 +310,16 @@ static gint remove_stale_lock_files ()
  *
  * @param argc argc from main
  * @param argv argv from main
+ * @param config_file pointer to config file path if specified via command-line
  * @return TRUE if we should show the configuration wizard, FALSE otherwise
  */
-static gboolean parse_cli (int argc, char *argv[])
+static gboolean parse_cli (int argc, char *argv[], gchar **config_file)
 {
     DEBUG_FUNCTION ("parse_cli");
     DEBUG_ASSERT (argc != 0);
     DEBUG_ASSERT (argv != NULL);
+    // *config_file must be non-null only if a configuration file path has been parsed
+    DEBUG_ASSERT (*config_file == NULL);
 
     /* Set default values */
     gchar *background_color = config_getstr ("background_color");
@@ -348,6 +351,7 @@ static gboolean parse_cli (int argc, char *argv[])
         { "command",            'c', 0, G_OPTION_ARG_STRING,    &command,           N_("Run a command at startup"), NULL },
         { "hidden",             'h', 0, G_OPTION_ARG_NONE,      &hidden,            N_("Start Tilda hidden"), NULL },
         { "font",               'f', 0, G_OPTION_ARG_STRING,    &font,              N_("Set the font to the following string"), NULL },
+        { "config-file",        'g', 0, G_OPTION_ARG_STRING,    config_file,        N_("Configuration file"), NULL },
         { "lines",              'l', 0, G_OPTION_ARG_INT,       &lines,             N_("Scrollback Lines"), NULL },
         { "scrollbar",          's', 0, G_OPTION_ARG_NONE,      &scrollbar,         N_("Use Scrollbar"), NULL },
         { "version",            'v', 0, G_OPTION_ARG_NONE,      &version,           N_("Print the version, then exit"), NULL },
@@ -364,13 +368,27 @@ static gboolean parse_cli (int argc, char *argv[])
         { NULL }
     };
 
-
     /* Set up the command-line parser */
     GError *error = NULL;
     GOptionContext *context = g_option_context_new (NULL);
     g_option_context_add_main_entries (context, cl_opts, NULL);
     g_option_context_parse (context, &argc, &argv, &error);
     g_option_context_free (context);
+
+    /* Check for mutually exclusive options, and exit gracefully if there are some */
+    if (*config_file != NULL) {
+        if (antialias || background_color || command || font || hidden || lines ||
+#ifdef VTE_290
+            image || transparency ||
+#else
+            back_alpha ||
+#endif
+            scrollbar || working_dir || x_pos || y_pos) {
+            g_printerr (_("Error: %s\n"),
+                   "Option 'config-file' is not compatible with any of the other options that can be found in a configuration file." );
+            exit (EXIT_FAILURE);
+        }
+    }
 
     /* Check for unknown options, and give a nice message if there are some */
     if (error)
@@ -720,7 +738,7 @@ int main (int argc, char *argv[])
     lock.pid = getpid ();
     lock.instance = get_instance_number ();
     lock_file = create_lock_file (&lock);
-    config_file = get_config_file_name (lock.instance);
+
     /* End of atomic section */
 
     flock(global_lock.file_descriptor, LOCK_UN);
@@ -747,11 +765,24 @@ int main (int argc, char *argv[])
         if (atol (getenv ("VTE_PROFILE_MEMORY")) != 0)
             g_mem_set_vtable (glib_mem_profiler_table);
 #endif
+    /* Parse the command line */
+    config_file = NULL;
+    need_wizard = parse_cli (argc, argv, &config_file);
+
+    if (config_file != NULL) {
+        if (!g_file_test (config_file, G_FILE_TEST_EXISTS)) {
+            g_printerr (_("Specified config file '%s' does not exist. Reverting to default path.\n"),
+                    config_file);
+            config_file = NULL;
+        }
+    }
+    if (config_file == NULL) {
+        config_file = get_config_file_name (lock.instance);
+    }
+
     /* Start up the configuration system */
     gint config_init_result = config_init (config_file);
 
-    /* Parse the command line */
-    need_wizard = parse_cli (argc, argv);
 
     /* We're about to startup X, so set the error handler. */
     XSetErrorHandler (xerror_handler);

commit 2bb49865ece319ff1de9426fa82363395e343922

Fix a segfault when tilda is started
Due to an error introduced in commit c85c87ff3fce9a637c3519f67485d0065e65f49e
the tilda config was read before it was initialized which lead to
a segfault.

Fixes #291

src/tilda.c
@@ -323,27 +323,27 @@ static gboolean parse_cli (int argc, char *argv[], tilda_cli_options *cli_option
     DEBUG_ASSERT (*config_file == NULL);
 
     /* Set default values */
-    cli_options->background_color = config_getstr ("background_color");
-    cli_options->command = config_getstr ("command");
-    cli_options->font = config_getstr ("font");
-    cli_options->working_dir = config_getstr ("working_dir");
+    cli_options->background_color = NULL;
+    cli_options->command = NULL;
+    cli_options->font = NULL;
+    cli_options->working_dir = NULL;
 
 #ifdef VTE_290
-    cli_options->image = config_getstr ("image");
-    cli_options->transparency = config_getint ("transparency");
+    cli_options->image = NULL;
+    cli_options->transparency = 0;
 #else
-    cli_options->back_alpha = config_getint ("back_alpha");
+    cli_options->back_alpha = 0;
 #endif
 
-    cli_options->lines = config_getint ("lines");
-    cli_options->x_pos = config_getint ("x_pos");
-    cli_options->y_pos = config_getint ("y_pos");
+    cli_options->lines = 0;
+    cli_options->x_pos = 0;
+    cli_options->y_pos = 0;
 
-    cli_options->antialias = config_getbool ("antialias");
-    cli_options->scrollbar = config_getbool ("scrollbar");
+    cli_options->antialias = FALSE;
+    cli_options->scrollbar = FALSE;
     cli_options->show_config = FALSE;
     cli_options->version = FALSE;
-    cli_options->hidden = config_getbool ("hidden");
+    cli_options->hidden = FALSE;
 
     /* All of the various command-line options */
     GOptionEntry cl_opts[] = {
@@ -352,7 +352,7 @@ static gboolean parse_cli (int argc, char *argv[], tilda_cli_options *cli_option
         { "command",            'c', 0, G_OPTION_ARG_STRING,    &(cli_options->command),           N_("Run a command at startup"), NULL },
         { "hidden",             'h', 0, G_OPTION_ARG_NONE,      &(cli_options->hidden),            N_("Start Tilda hidden"), NULL },
         { "font",               'f', 0, G_OPTION_ARG_STRING,    &(cli_options->font),              N_("Set the font to the following string"), NULL },
-        { "config-file",        'g', 0, G_OPTION_ARG_STRING,    config_file,        N_("Configuration file"), NULL },
+        { "config-file",        'g', 0, G_OPTION_ARG_STRING,    config_file,                       N_("Configuration file"), NULL },
         { "lines",              'l', 0, G_OPTION_ARG_INT,       &(cli_options->lines),             N_("Scrollback Lines"), NULL },
         { "scrollbar",          's', 0, G_OPTION_ARG_NONE,      &(cli_options->scrollbar),         N_("Use Scrollbar"), NULL },
         { "version",            'v', 0, G_OPTION_ARG_NONE,      &(cli_options->version),           N_("Print the version, then exit"), NULL },
@@ -786,23 +786,24 @@ int main (int argc, char *argv[])
         if (atol (getenv ("VTE_PROFILE_MEMORY")) != 0)
             g_mem_set_vtable (glib_mem_profiler_table);
 #endif
-    /* Parse the command line */
+
     config_file = NULL;
+
+    /* Parse the command line */
     tilda_cli_options *cli_options = init_cli_options();
     need_wizard = parse_cli (argc, argv, cli_options, &config_file);
 
-    if (config_file != NULL) {
+    if (config_file) {	  // if there was a config file specified via cli
         if (!g_file_test (config_file, G_FILE_TEST_EXISTS)) {
             g_printerr (_("Specified config file '%s' does not exist. Reverting to default path.\n"),
                     config_file);
             config_file = NULL;
         }
-    }
-    if (config_file == NULL) {
+    } else {    // if not, we look for the defaut config file
         config_file = get_config_file_name (lock.instance);
     }
 
-    /* Start up the configuration system */
+    /* Start up the configuration system and load from file */
     gint config_init_result = config_init (config_file);
 
     /* Set up possible overridden config options */
@@ -911,4 +912,3 @@ initialization_failed:
     g_free (config_file);
     return 0;
 }
-