1c01631ef0660fe3ba79e286336ac514de8a6398
Sebastian Geiger <sbastig@gmx.net>
2020-05-03 15:24:41 +0200

    context-menu: correctly free link
    This fixes a bug introduced in commit c8f7fc4f which lead to memory not
    being freed, when the context menu was closed without using the
    "copy-link" action.
    
    This change introduces a separate structure that keeps a reference to
    the matched string and ensures that it is freed after the menu
    selection has been done.

--- src/tilda-context-menu.c
+++ src/tilda-context-menu.c
@@ -4,6 +4,15 @@
 #include "wizard.h"
 #include <vte/vte.h>
 
+typedef struct {
+    tilda_window * tw;
+    tilda_term * tt;
+    char * match;
+} TildaContextMenu;
+
+static void
+tilda_context_menu_free (TildaContextMenu * context_menu);
+
 static void
 menu_add_tab_cb (GSimpleAction *action,
                  GVariant      *parameter,
@@ -65,8 +74,6 @@ menu_copy_link_cb (GSimpleAction * action,
     GtkClipboard * clipBoard = gtk_clipboard_get_default (gdk_display_get_default ());
 
     gtk_clipboard_set_text (clipBoard, link, -1);
-
-    g_free (link);
 }
 
 static void
@@ -113,21 +120,26 @@ menu_quit_cb (GSimpleAction *action,
     tilda_window_confirm_quit (TILDA_WINDOW (user_data));
 }
 
-static void on_popup_hide (GtkWidget *widget, tilda_window *tw)
+static void on_popup_hide (GtkWidget *widget, TildaContextMenu * context_menu)
 {
     DEBUG_FUNCTION ("on_popup_hide");
     DEBUG_ASSERT (widget != NULL);
-    DEBUG_ASSERT (tw != NULL);
+    DEBUG_ASSERT (context_menu != NULL);
+    DEBUG_ASSERT (context_menu->tw != NULL);
+
+    tilda_window * tw = context_menu->tw;
 
     tw->disable_auto_hide = FALSE;
 }
 
 static void
-on_selection_done (GtkWidget *widget, tilda_window *tw)
+on_selection_done (GtkWidget *widget, TildaContextMenu * context_menu)
 {
     DEBUG_FUNCTION ("on_selection_done");
 
     gtk_menu_detach (GTK_MENU (widget));
+
+    tilda_context_menu_free (context_menu);
 }
 
 void
@@ -136,6 +148,10 @@ tilda_context_menu_popup (tilda_window *tw, tilda_term *tt, GdkEvent * event)
     DEBUG_FUNCTION ("popup_menu");
     DEBUG_ASSERT (tw != NULL);
     DEBUG_ASSERT (tt != NULL);
+
+    TildaContextMenu * context_menu = g_new0 (TildaContextMenu, 1);
+    context_menu->tw = tw;
+    context_menu->tt = tt;
 
     GtkBuilder *builder = gtk_builder_new ();
 
@@ -177,15 +193,16 @@ tilda_context_menu_popup (tilda_window *tw, tilda_term *tt, GdkEvent * event)
                                      entries_for_tilda_terminal, G_N_ELEMENTS (entries_for_tilda_terminal), tt);
 
     char * match = vte_terminal_match_check_event (VTE_TERMINAL (tt->vte_term), event, NULL);
+    context_menu->match = g_strdup (match);
 
     g_action_map_add_action_entries (G_ACTION_MAP (action_group),
-                                     entries_for_regex, G_N_ELEMENTS (entries_for_regex), match);
+                                     entries_for_regex, G_N_ELEMENTS (entries_for_regex), context_menu->match);
 
     GAction *copyAction;
 
     copyAction = g_action_map_lookup_action (G_ACTION_MAP (action_group), "copy-link");
 
-    if (match == NULL) {
+    if (context_menu->match == NULL) {
         g_simple_action_set_enabled (G_SIMPLE_ACTION (copyAction), FALSE);
     }
 
@@ -201,11 +218,18 @@ tilda_context_menu_popup (tilda_window *tw, tilda_term *tt, GdkEvent * event)
 
     /* Disable auto hide */
     tw->disable_auto_hide = TRUE;
-    g_signal_connect (G_OBJECT (menu), "unmap", G_CALLBACK (on_popup_hide), tw);
-    g_signal_connect (G_OBJECT (menu), "selection-done", G_CALLBACK (on_selection_done), tw);
+    g_signal_connect (G_OBJECT (menu), "unmap", G_CALLBACK (on_popup_hide), context_menu);
+    g_signal_connect (G_OBJECT (menu), "selection-done", G_CALLBACK (on_selection_done), context_menu);
 
     gtk_menu_popup_at_pointer (GTK_MENU (menu), event);
 
     g_object_unref (action_group);
     g_object_unref (builder);
+}
+
+static void
+tilda_context_menu_free (TildaContextMenu * context_menu)
+{
+    g_free (context_menu->match);
+    g_free (context_menu);
 }
