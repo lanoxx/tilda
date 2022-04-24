51a980a55ad6d750daa21d43a66d44577dad277b
xtkoba <69125751+xtkoba@users.noreply.github.com>
2022-04-24 11:32:41 +0900

    Fix OOB write in `start_default_shell()`

--- src/tilda_terminal.c
+++ src/tilda_terminal.c
@@ -573,7 +573,7 @@ start_default_shell (tilda_term *tt)
          */
         flags |= G_SPAWN_FILE_AND_ARGV_ZERO;
     } else {
-        argv = malloc(1 * sizeof(void *));
+        argv = malloc(2 * sizeof(void *));
         argv[0] = default_command;
         argv[1] = NULL;
     }
