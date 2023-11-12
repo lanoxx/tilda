lanoxx/tilda

commit 42139b7ec41d7fa168d7beff54a1a7b3bffccc79

Wizard: Add a "Use login shell option"
 * The use login shell option works only for shells,
   not for custom commands. When the "Use login shell"
   option is activated, then the shell in each tab is
   started as a login shell.

src/tilda_terminal.c
@@ -459,22 +459,37 @@ launch_default_shell:
     /* We need to create a NULL terminated list of arguments.
      * The first item is the command to execute in the shell, in this
      * case there are no further arguments being passed. */
-    argv = malloc(2 * sizeof(void *));
-    argv[0] = default_command;
-    argv[1] = NULL;
+    GSpawnFlags flags = 0;
+    gchar* argv1 = NULL;
+    if(config_getbool("command_login_shell")) {
+        argv1 = g_strdup_printf("-%s", default_command);
+        argv = malloc(3 * sizeof(void *));
+        argv[0] = default_command;
+        argv[1] = argv1;
+        argv[2] = NULL;
+        /* This is needed so that argv[1] becomes the argv[0] of the new process. Otherwise
+         * glib just duplicates argv[0] when it executes the command and it is not possible
+         * to modify the argv[0] that the new command sees.
+         */
+        flags |= G_SPAWN_FILE_AND_ARGV_ZERO;
+    } else {
+        argv = malloc(1 * sizeof(void *));
+        argv[0] = default_command;
+        argv[1] = NULL;
+    }
 
     ret = vte_terminal_fork_command_full (VTE_TERMINAL (tt->vte_term),
         VTE_PTY_DEFAULT, /* VtePtyFlags pty_flags */
         working_dir, /* const char *working_directory */
         argv, /* char **argv */
         NULL, /* char **envv */
-        0,    /* GSpawnFlags spawn_flags */
+        flags,    /* GSpawnFlags spawn_flags */
         NULL, /* GSpawnChildSetupFunc child_setup */
         NULL, /* gpointer child_setup_data */
         &tt->pid, /* GPid *child_pid */
         NULL  /* GError **error */
         );
-
+    g_free(argv1);
     g_free (argv);
 
     if (ret == -1)

src/configsys.c
@@ -158,6 +158,7 @@ static cfg_opt_t config_opts[] = {
     CFG_BOOL("title_max_length_flag", TRUE, CFGF_NONE),
     /* Whether to set a new tab's working dir to the current tab's */
     CFG_BOOL("inherit_working_dir", TRUE, CFGF_NONE),
+    CFG_BOOL("command_login_shell", FALSE, CFGF_NONE),
     CFG_END()
 };
 

src/tilda.ui
@@ -1222,7 +1222,7 @@
                           <object class="GtkTable" id="table_command">
                             <property name="visible">True</property>
                             <property name="can_focus">False</property>
-                            <property name="n_rows">3</property>
+                            <property name="n_rows">4</property>
                             <property name="n_columns">2</property>
                             <property name="homogeneous">True</property>
                             <child>
@@ -1298,6 +1298,25 @@
                                 <property name="right_attach">2</property>
                                 <property name="top_attach">1</property>
                                 <property name="bottom_attach">2</property>
+                                <property name="x_padding">4</property>
+                                <property name="y_padding">4</property>
+                              </packing>
+                            </child>
+                            <child>
+                              <placeholder/>
+                            </child>
+                            <child>
+                              <object class="GtkCheckButton" id="check_command_login_shell">
+                                <property name="label" translatable="yes">Use a login shell</property>
+                                <property name="visible">True</property>
+                                <property name="can_focus">True</property>
+                                <property name="receives_default">False</property>
+                                <property name="xalign">0</property>
+                                <property name="draw_indicator">True</property>
+                              </object>
+                              <packing>
+                                <property name="top_attach">3</property>
+                                <property name="bottom_attach">4</property>
                                 <property name="x_padding">4</property>
                                 <property name="y_padding">4</property>
                               </packing>

src/wizard.c
@@ -996,6 +996,7 @@ static void check_run_custom_command_toggled_cb (GtkWidget *w)
     const gboolean status = gtk_toggle_button_get_active (GTK_TOGGLE_BUTTON(w));
     GtkWidget *label_custom_command;
     GtkWidget *entry_custom_command;
+    GtkWidget *check_command_login_shell;
 
     config_setbool ("run_command", status);
 
@@ -1003,9 +1004,19 @@ static void check_run_custom_command_toggled_cb (GtkWidget *w)
         GTK_WIDGET (gtk_builder_get_object (xml, "label_custom_command"));
     entry_custom_command =
         GTK_WIDGET (gtk_builder_get_object (xml, "entry_custom_command"));
+    check_command_login_shell =
+        GTK_WIDGET (gtk_builder_get_object (xml, "check_command_login_shell"));
 
     gtk_widget_set_sensitive (label_custom_command, status);
     gtk_widget_set_sensitive (entry_custom_command, status);
+    gtk_widget_set_sensitive (check_command_login_shell, !status);
+
+    if(!status) {
+        gtk_entry_set_icon_from_icon_name(GTK_ENTRY(entry_custom_command),
+                GTK_ENTRY_ICON_SECONDARY,
+                NULL);
+    }
+
     gtk_widget_grab_focus(entry_custom_command);
 }
 
@@ -1055,6 +1066,12 @@ static void combo_command_exit_changed_cb (GtkWidget *w) {
     const gint status = gtk_combo_box_get_active (GTK_COMBO_BOX(w));
 
     config_setint ("command_exit", status);
+}
+
+static void check_command_login_shell_cb (GtkWidget *w) {
+    const gboolean active = gtk_toggle_button_get_active(GTK_TOGGLE_BUTTON(w));
+
+    config_setbool("command_login_shell", active);
 }
 
 static void combo_on_last_terminal_exit_changed_cb (GtkWidget *w)
@@ -2049,6 +2066,7 @@ static void set_wizard_state_from_config () {
 
     CHECK_BUTTON ("check_run_custom_command", "run_command");
     TEXT_ENTRY ("entry_custom_command", "command");
+    CHECK_BUTTON ("check_command_login_shell", "command_login_shell");
     COMBO_BOX ("combo_command_exit", "command_exit");
     COMBO_BOX ("combo_on_last_terminal_exit", "on_last_terminal_exit");
     SET_SENSITIVE_BY_CONFIG_BOOL ("entry_custom_command","run_command");
@@ -2186,6 +2204,7 @@ static void connect_wizard_signals ()
     CONNECT_SIGNAL ("check_run_custom_command","toggled",check_run_custom_command_toggled_cb);
     CONNECT_SIGNAL ("entry_custom_command","focus-out-event", validate_executable_command_cb);
     CONNECT_SIGNAL ("combo_command_exit","changed",combo_command_exit_changed_cb);
+    CONNECT_SIGNAL ("check_command_login_shell", "toggled", check_command_login_shell_cb);
 
     CONNECT_SIGNAL ("entry_web_browser","changed",entry_web_browser_changed);
     CONNECT_SIGNAL ("entry_web_browser","focus-out-event", validate_executable_command_cb);

commit d09139e65f69ce5a3634458b187839692ee10716

Fix OOB write in `start_default_shell()`
This fixes an out-of-bounds regression issue in the start_default_shell
function which was introduced in commit
42139b7ec41d7fa168d7beff54a1a7b3bffccc79.

(cherry picked from commit 51a980a55ad6d750daa21d43a66d44577dad277b)
Signed-off-by: Sebastian Geiger <sbastig@gmx.net>

src/tilda_terminal.c
@@ -532,7 +532,7 @@ start_default_shell (tilda_term *tt)
          */
         flags |= G_SPAWN_FILE_AND_ARGV_ZERO;
     } else {
-        argv = malloc(1 * sizeof(void *));
+        argv = malloc(2 * sizeof(void *));
         argv[0] = default_command;
         argv[1] = NULL;
     }